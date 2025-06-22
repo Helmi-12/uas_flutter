255e5621f3a3d4bff89a8fd12effe9d99f47d592
Helmi-12 <bilipick4444@gmail.com>
2025-06-22 21:58:49 +0700

    menambahkan class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:percobaan/login_page.dart';
 import 'package:percobaan/splash_page.dart';
+import 'package:percobaan/beranda_page.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
